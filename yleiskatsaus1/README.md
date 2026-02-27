2e6960895e11d123bcd9f05b0fc5ec99f00e307c
Eero <s2401329@edu.bc.fi>
2026-02-27 09:36:50 +0200

    Uusi kansio

---
+++ yleiskatsaus1/README.md
@@ -0,0 +1,2 @@
+# OhjelmistokomponentitREAL
+Tehtävät
---
+++ yleiskatsaus1/dekonstruktio/dekonstruktio&spread.js
@@ -0,0 +1,48 @@
+const numerot = [10, 20, 30, 40];
+
+let eka = numerot[0];
+
+let loput = numerot.slice(1);
+
+console.log(eka); // 10
+
+console.log(loput); // [20, 30, 40]
+
+const opiskelija = {
+  nimi: "Ville",
+  ika: 34,
+  koulutus: "Datanomi",
+  kaupunki: "Helsinki",
+};
+
+const { nimi, ika, koulutus, kaupunki } = opiskelija;
+
+console.log(nimi);
+console.log(ika);
+let muutTiedot = { koulutus, kaupunki };
+
+console.log(muutTiedot);
+
+const backend = ["Node.js", "SQL"];
+const frontend = ["HTML", "CSS", "JavaScript"];
+const fullstack = [...backend, ...frontend, "Git"];
+console.log(fullstack);
+
+const perusTiedot = {
+  nimi: "Anna",
+  ika: 19,
+};
+
+const lisatiedot = {
+  kaupunki: "Espoo",
+  harrastus: "Ohjelmointi",
+};
+const aktiivinen = true;
+
+const kaytaja = {
+  ...perusTiedot,
+  ...lisatiedot,
+  aktiivinen,
+};
+
+console.log(kaytaja);
